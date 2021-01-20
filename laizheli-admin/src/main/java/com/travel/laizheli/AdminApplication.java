9534a1b85fb4a8a7a0c2a4974623b1df81993016
unknown <596151248@qq.com>
2021-01-20 16:19:42 +0800

    admin和agent

---
+++ laizheli-admin/src/main/java/com/travel/laizheli/AdminApplication.java
@@ -0,0 +1,15 @@
+package com.travel.laizheli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 管理端启动项
+ * create by chen on 2021/1/20
+ */
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
---
+++ laizheli-agent/src/main/java/com/travel/laizheli/AgentApplication.java
@@ -0,0 +1,16 @@
+package com.travel.laizheli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 供应商端启动项
+ * create by chen on 2021/1/20
+ */
+@SpringBootApplication
+//@MapperScan("com.travel.laizheli.mapper")
+public class AgentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentApplication.class, args);
+    }
+}
