10ede3920359ad657e84f52d1d862aeb58f85795
blknemo <blknemo@163.com>
2021-01-22 12:58:16 +0800

    完成登录功能

---
+++ laizheli-client/src/main/java/com/travel/laizheli/Entity/LoginLog.java
@@ -0,0 +1,86 @@
+package com.travel.laizheli.Entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginLog {
+
+  private long id;
+  private String userId;
+  private Date createTime;
+  private String ip;
+  private String os;
+  private String browser;
+  private String system;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+
+  public Date getCreateTime() {
+    return createTime;
+  }
+
+  public void setCreateTime(Date createTime) {
+    this.createTime = createTime;
+  }
+
+
+  public String getIp() {
+    return ip;
+  }
+
+  public void setIp(String ip) {
+    this.ip = ip;
+  }
+
+
+  public String getOs() {
+    return os;
+  }
+
+  public void setOs(String os) {
+    this.os = os;
+  }
+
+
+  public String getBrowser() {
+    return browser;
+  }
+
+  public void setBrowser(String browser) {
+    this.browser = browser;
+  }
+
+
+  public String getSystem() {
+    return system;
+  }
+
+  public void setSystem(String system) {
+    this.system = system;
+  }
+
+}
--- laizheli-client/src/main/java/com/travel/laizheli/Entity/User.java
+++ laizheli-client/src/main/java/com/travel/laizheli/Entity/User.java
@@ -1,6 +1,9 @@
 package com.travel.laizheli.Entity;
 
+import cn.hutool.core.date.DateTime;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
@@ -9,6 +12,8 @@ import java.util.Date;
  * create by chen on 2021/1/13
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     private String id;
     private String name;
